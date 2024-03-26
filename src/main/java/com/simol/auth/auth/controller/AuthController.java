9e665bbf6948530dee4e508f718380283010e456
juno-choi <ililil9482@naver.com>
2024-03-26 23:14:10 +0900

    fix : url 수정

--- src/main/java/com/simol/auth/auth/controller/AuthController.java
+++ src/main/java/com/simol/auth/auth/controller/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RestController
 @Slf4j
 public class AuthController {
